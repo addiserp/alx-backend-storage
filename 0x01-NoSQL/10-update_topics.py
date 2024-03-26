998ec17d49f5199998bc823678cc8bca3b3c914b
Mikias Gedlu <addiserp@gmail.com>
2024-03-26 18:51:49 +0300

    0x01. NoSQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    a Python function that changes all topics of a school
+    document based on the name:
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    a Python function that changes all topics of a school
+    document based on the name:
+
+    :param mongo_collection:
+    :param name:
+    :param topics:
+    :return:
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
